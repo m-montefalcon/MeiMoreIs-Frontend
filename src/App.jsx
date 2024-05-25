6bb146f12530f5fa5cbb7bcbde4066cdd4bf42a1
m-montefalcon <mmontefalconschlprps@gmail.com>
2024-05-25 22:01:13 +0800

    Implement axios in landing page first

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
         <Route path="/home" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-      </Routes>    b   
+      </Routes>
     </BrowserRouter>
   );
 }
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,14 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import HomePage from "./HomePage";
 import LoginPage from "./LoginPage";
+import axios from "axios";
+
 const LandingPage = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const navigateTo = useNavigate();
 
-  const authRender = () => {
-    return isAuthenticated ? <HomePage /> : <LoginPage />;
-  };
+  useEffect(() => {
+    const checkAuthentication = async () => {
+      try {
+        const result = await axios.get("http://localhost:8000");
+        if (result.status === 200) {
+          setIsAuthenticated(true);
+        } else {
+          setIsAuthenticated(false);
+          navigateTo("/login");
+        }
+      } catch (error) {
+        console.error("Error:", error);
+        setIsAuthenticated(false);
+        navigateTo("/login");
+      }
+    };
 
-  return <>{authRender()}</>;
+    checkAuthentication();
+  }, []); // Empty dependency array ensures the effect runs only once
+
+  return isAuthenticated ? <HomePage /> : <LoginPage />;
 };
 
 export default LandingPage;
