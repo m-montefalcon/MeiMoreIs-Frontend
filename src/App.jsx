08c7644fcbf9aaf95b4126b886fdbfaf1eee9706
m-montefalcon <mmontefalconschlprps@gmail.com>
2024-06-11 23:08:41 +0800

    Boileplate for profile page

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
-import React, { useEffect } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useEffect } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
-import HomePage from "./pages/HomePage";
-import "./styles/App.css";
-import LandingPage from "./pages/LandingPage";
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+import HomePage from './pages/HomePage'
+import './styles/App.css'
+import LandingPage from './pages/LandingPage'
+import ProfilePage from './pages/ProfilePage'
 
 function App() {
-  useEffect(() => {
-    window.history.scrollRestoration = "manual";
-  }, []);
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} exact />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    useEffect(() => {
+        window.history.scrollRestoration = 'manual'
+    }, [])
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<LandingPage />} exact />
+                <Route path="/home" element={<HomePage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/profile" element={<ProfilePage />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
-export default App;
+export default App
