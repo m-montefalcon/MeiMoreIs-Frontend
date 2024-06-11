76165b125f584d9467020133cf67295443d3ffb0
m-montefalcon <mmontefalconschlprps@gmail.com>
2024-06-11 21:54:57 +0800

    Fix routing on being refreshed and route on the navbar

--- src/components/home_components/NavBarComponent.jsx
+++ src/components/home_components/NavBarComponent.jsx
@@ -1,40 +1,47 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { removeUserDataFromLocalStorage } from "../../util/localStorageUtils.js";
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { removeUserDataFromLocalStorage } from '../../util/localStorageUtils.js'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 const NavBarComponent = (props) => {
-  const navigateTo = useNavigate();
-  const refreshPage = () => {
-    window.location.reload(); // Reload the page
-  };
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  const handleClick = () => {
+    if (location.pathname === '/home') {
+      window.location.reload() // Reload the page if already on /home
+    } else {
+      navigate('/home') // Navigate to the home page otherwise
+    }
+  }
 
   const onLogout = () => {
-    removeUserDataFromLocalStorage();
-    navigateTo("/login");
-  };
+    removeUserDataFromLocalStorage()
+    navigate('/login')
+  }
+
   return (
     <Navbar
       expand="lg"
       style={{
-        backgroundColor: "#f9f9f9",
-        position: "fixed",
+        backgroundColor: '#f9f9f9',
+        position: 'fixed',
         top: 0,
-        width: "100%",
+        width: '100%',
         zIndex: 1000,
       }}
     >
       <Container>
         <Nav className="me-auto"></Nav>
-        <Navbar.Brand href="#home">MeiMoreIs</Navbar.Brand>
+        <Navbar.Brand>MeiMoreIs</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link onClick={refreshPage}>Home</Nav.Link>{" "}
+            <Nav.Link onClick={handleClick}>Home</Nav.Link>{' '}
             <NavDropdown
               title={
                 <img
@@ -42,19 +49,21 @@ const NavBarComponent = (props) => {
                   src={props.image}
                   className="rounded-circle"
                   alt="User"
-                  style={{ width: "20px", height: "20px" }}
+                  style={{ width: '20px', height: '20px' }}
                 />
               }
               id="basic-nav-dropdown"
             >
-              <NavDropdown.Item>{props.name}</NavDropdown.Item>
+              <NavDropdown.Item onClick={() => navigate('/profile')}>
+                {props.name}
+              </NavDropdown.Item>
               <NavDropdown.Item onClick={onLogout}>Logout</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  );
-};
+  )
+}
 
-export default NavBarComponent;
+export default NavBarComponent
